e8ba8f963182b93484476257b31c5118091a71e2
Dzy-HW-XD <462461328@qq.com>
2021-02-24 02:05:06 +0800

    Update index.php

--- F3-SimpleExample-code-Anon/DeadPeopleSystem/index.php
+++ F3-SimpleExample-code-Anon/DeadPeopleSystem/index.php
@@ -169,7 +169,7 @@ $f3->route('POST /search',
                 break;
         }
 
-        $list = $f3->get('DB')->exec("SELECT * FROM deadinfo WHERE (age BETWEEN'$agelow'and'$agehigh') AND (sex='$s') AND (height BETWEEN'$heightlow'and'$heighthigh') AND (height='$h') AND (tattoos='$t') AND (birthmark='$b') AND (timeofdeath='tofdeadth')");
+        $list = $f3->get('DB')->exec("SELECT * FROM deadinfo WHERE (age BETWEEN'$agelow'and'$agehigh') AND (sex='$s') AND (height BETWEEN'$heightlow'and'$heighthigh') AND (height='$h') AND (tattoos='$t') AND (birthmark='$b') AND (timeofdeath='$tofdeadth')");
 
         $f3->set('result',$list);
         echo template::instance()->render('search.html');
